d7701d37d58a73fb3de1fa933b5004d6d209fc2c
fabilou <hello@fabilou.eu>
2025-02-28 19:04:15 +0100

    Update Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,22 @@
 import * as React from "react"
 import { graphql, Link, useStaticQuery } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+
+import { useGlobalContext } from "./GlobalContext"
+import Thumbnail from "./Thumbnail"
 
 import * as styles from "./Projects.module.sass"
 
-const Projects: React.FC = () => {
+interface ProjectsProps {
+	filter?: string
+}
+
+const Projects: React.FC<ProjectsProps> = ({ filter }) => {
 	const data = useStaticQuery(graphql`
 		query {
 			allGalleryYaml {
 				edges {
 					node {
-						link
-						tags
+						slug
 						thumbnail {
 							childImageSharp {
 								gatsbyImageData
@@ -29,88 +34,98 @@ const Projects: React.FC = () => {
 		}
 	`)
 
+	const { projectInfo } = useGlobalContext()
+
 	const [grabbing, setGrabbing] = React.useState(false)
 	const [scrollStart, setScrollStart] = React.useState(0)
 	const [scroll, setScroll] = React.useState(0)
 
+	const containerRef = React.useRef<HTMLDivElement>(null)
+	const thumbnailRef = React.useRef<HTMLDivElement>(null)
+
+	React.useEffect(() => {
+		if (containerRef.current) {
+			containerRef.current.scrollLeft = 0
+		}
+	}, [])
+
+	React.useEffect(() => {
+		const container = containerRef.current
+		if (!container) return
+
+		const handleWheel = (e: WheelEvent) => {
+			container.scrollLeft += e.deltaY
+		}
+
+		container.addEventListener("wheel", handleWheel)
+
+		return () => {
+			container.removeEventListener("wheel", handleWheel)
+		}
+	}, [])
+
+	React.useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			console.log(entries.map((entry) => entry.target))
+		})
+
+		if (thumbnailRef.current) {
+			console.log(thumbnailRef.current)
+			observer.observe(thumbnailRef.current)
+		}
+
+		return () => {
+			if (thumbnailRef.current) {
+				observer.unobserve(thumbnailRef.current)
+			}
+		}
+	}, [])
+
 	return (
-		<div
-			className={styles.container}
-			onPointerDown={(e) => {
-				e.preventDefault()
-
-				setGrabbing(true)
-				setScrollStart(e.pageX)
-				setScroll(e.currentTarget.scrollLeft)
-
-				e.currentTarget.classList.add(styles.grabbing)
-			}}
-			onPointerUp={(e) => {
-				setGrabbing(false)
-
-				e.currentTarget.classList.remove(styles.grabbing)
-			}}
-			onPointerLeave={(e) => {
-				setGrabbing(false)
-
-				e.currentTarget.classList.remove(styles.grabbing)
-			}}
-			onPointerMove={(e) => {
-				if (!grabbing) return
-
-				e.preventDefault()
-
-				const x = e.pageX
-				const move = x - scrollStart
-
-				e.currentTarget.scrollLeft = scroll - move
-			}}
-		>
-			{data.allGalleryYaml.edges.map((item: any, i: number) => (
-				<figure
-					className={styles.thumbnail}
-					onPointerEnter={(e) => e.currentTarget.classList.add(styles.hover)}
-					onPointerLeave={(e) => e.currentTarget.classList.remove(styles.hover)}
-					key={i}
-					style={{
-						flexBasis: `calc(${item.node.width} * 360px)`,
-					}}
-				>
-					{item.node.thumbnail.internal.mediaType === "video/mp4" ||
-					item.node.thumbnail.internal.mediaType === "video/webm" ? (
-						<div>
-							<video
-								autoPlay
-								controls={false}
-								controlsList="nodownload"
-								disablePictureInPicture
-								loop
-								muted
-								playsInline
-							>
-								<source
-									src={item.node.thumbnail.publicURL}
-									type={item.node.thumbnail.internal.mediaType}
-								/>
-							</video>
-						</div>
-					) : item.node.thumbnail.internal.mediaType === "image/jpeg" ||
-					  item.node.thumbnail.internal.mediaType === "image/png" ||
-					  item.node.thumbnail.internal.mediaType === "image/webp" ||
-					  item.node.thumbnail.internal.mediaType === "image/gif" ||
-					  item.node.thumbnail.internal.mediaType === "image/svg" ? (
-						<GatsbyImage
-							image={item.node.thumbnail.childImageSharp.gatsbyImageData}
-							alt=""
-						/>
-					) : (
-						<></>
-					)}
-					<figcaption className={styles.description}>
-						{item.node.title}
-					</figcaption>
-				</figure>
-			))}
+		<div>
+			<div
+				ref={containerRef}
+				className={styles.container}
+				onPointerDown={(e) => {
+					e.preventDefault()
+					setGrabbing(true)
+					setScrollStart(e.pageX)
+					setScroll(e.currentTarget.scrollLeft)
+					e.currentTarget.classList.add(styles.grabbing)
+				}}
+				onPointerUp={(e) => {
+					setGrabbing(false)
+					e.currentTarget.classList.remove(styles.grabbing)
+				}}
+				onPointerLeave={(e) => {
+					setGrabbing(false)
+					e.currentTarget.classList.remove(styles.grabbing)
+				}}
+				onPointerMove={(e) => {
+					if (!grabbing) return
+					e.preventDefault()
+					const x = e.pageX
+					const move = x - scrollStart
+					e.currentTarget.scrollLeft = scroll - move
+				}}
+			>
+				{projectInfo ? projectInfo : null}
+				{data.allGalleryYaml.edges.map((item: any, i: number) => (
+					<Thumbnail
+						caption={item.node.title ? item.node.title : null}
+						hidden={filter !== undefined && item.node.slug !== filter}
+						image={
+							item.node.thumbnail.childImageSharp
+								? item.node.thumbnail.childImageSharp.gatsbyImageData
+								: item.node.thumbnail.publicURL
+						}
+						key={i}
+						slug={item.node.slug ? item.node.slug : null}
+						type={item.node.thumbnail.internal.mediaType}
+						width={item.node.width}
+					/>
+				))}
+			</div>
 		</div>
 	)
 }
