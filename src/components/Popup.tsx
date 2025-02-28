abc1c121afe633472a4b505f5b29e0b6cefce03c
fabilou <hello@fabilou.eu>
2025-02-28 19:03:20 +0100

    Update Popup component

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,63 +1,41 @@
 import * as React from "react"
-import { Link, navigate } from "gatsby"
+import { navigate } from "gatsby"
+
+import { useGlobalContext } from "./GlobalContext"
 
 import * as styles from "./Popup.module.sass"
 
 interface PopupProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const Popup: React.FC<PopupProps> = ({ children }) => {
+	const { projectURL } = useGlobalContext()
+
 	const popup = React.useRef<HTMLDivElement>(null)
 	const popupContainer = React.useRef<HTMLDivElement>(null)
 
-	const [bounds, setBounds] = React.useState({
-		top: 0,
-		height: 0,
-		left: 0,
-		width: 0,
-	})
-	const [isInside, setIsInside] = React.useState(true)
-
-	React.useEffect(() => {
-		const updateBounds = () =>
-			popup.current && setBounds(popup.current?.getBoundingClientRect())
-
-		updateBounds()
-
-		window.addEventListener("resize", () => updateBounds())
-
-		return window.removeEventListener("resize", updateBounds)
-	}, [])
+	const [isInside, setIsInside] = React.useState(false)
 
 	const closePopup = () => {
 		popup.current?.classList.add(styles.close)
 		popupContainer.current?.addEventListener("animationend", () =>
-			navigate("/")
+			navigate(projectURL ? projectURL : "/")
 		)
 	}
 
-	const handlePointerDown = (e: React.PointerEvent) => {
+	const handleMouseDown = (e: React.MouseEvent) => {
 		if (!isInside) {
 			closePopup()
 		}
 	}
 
-	const handlePointerMove = (e: React.PointerEvent) => {
-		setIsInside(
-			bounds &&
-				bounds.top <= e.clientY &&
-				e.clientY <= bounds.top + bounds.height &&
-				bounds.left <= e.clientX &&
-				e.clientX <= bounds.left + bounds.width
-		)
-	}
-
 	return (
-		<div
-			onPointerDown={(e) => handlePointerDown(e)}
-			onPointerMove={(e) => handlePointerMove(e)}
-			ref={popupContainer}
-		>
-			<div className={styles.container} ref={popup}>
+		<div onMouseDown={(e) => handleMouseDown(e)} ref={popupContainer}>
+			<div
+				className={styles.container}
+				ref={popup}
+				onMouseEnter={() => setIsInside(true)}
+				onMouseLeave={() => setIsInside(false)}
+			>
 				<button className={styles.button} onClick={() => closePopup()}>
 					Close
 				</button>
