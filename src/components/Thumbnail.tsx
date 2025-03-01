04ee25757f220ef97747c8cae42bc777de5b8f29
fabilou <hello@fabilou.eu>
2025-03-01 10:47:49 +0100

    Update Thumbnail component

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,67 +1,18 @@
-import React from "react"
+import React, { ForwardedRef, forwardRef } from "react"
 import { Link } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 import * as styles from "./Thumbnail.module.sass"
 
-interface ThumbnailProps {
-	caption?: string
-	image: any
-	hidden?: boolean
-	slug?: string
-	type: string
-	width?: number
+interface ImageProps {
+	img: any
+	mediaType: string
 }
 
-const Thumbnail: React.FC<ThumbnailProps> = ({
-	caption,
-	image,
-	hidden,
-	slug,
-	type,
-	width,
-}) => {
-	const thumbnail = React.useRef<HTMLDivElement>(null)
-	const video = React.useRef<HTMLVideoElement>(null)
-
-	const [visible, setVisible] = React.useState(true)
-
-	React.useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					setVisible(true)
-				} else {
-					setVisible(false)
-				}
-			})
-		})
-
-		if (thumbnail.current) {
-			observer.observe(thumbnail.current)
-		}
-
-		return () => {
-			if (thumbnail.current) {
-				observer.unobserve(thumbnail.current)
-			}
-		}
-	}, [])
-
-	return (
-		<figure
-			className={styles.thumbnail}
-			hidden={hidden}
-			onPointerEnter={(e) => e.currentTarget.classList.add(styles.hover)}
-			onPointerLeave={(e) => e.currentTarget.classList.remove(styles.hover)}
-			ref={thumbnail}
-			style={{
-				flexBasis: `min(min(${
-					width ? width : 1
-				} * var(--column-width), 75vw), 50vh)`,
-			}}
-		>
-			{type === "video/mp4" || type === "video/webm" ? (
+const Image: React.FC<ImageProps> = React.forwardRef(
+	({ img, mediaType }, ref: ForwardedRef<HTMLVideoElement>) => {
+		if (mediaType === "video/mp4" || mediaType === "video/webm") {
+			return (
 				<video
 					autoPlay
 					controls={false}
@@ -70,36 +21,99 @@ const Thumbnail: React.FC<ThumbnailProps> = ({
 					loop
 					muted
 					playsInline
-					ref={video}
+					ref={ref}
 				>
-					<source src={visible ? image : ""} type={type} />
+					<source src={img} type={mediaType} />
 				</video>
-			) : type === "image/jpeg" ||
-			  type === "image/png" ||
-			  type === "image/webp" ||
-			  type === "image/gif" ||
-			  type === "image/svg" ? (
-				<GatsbyImage image={image} alt="#" />
-			) : (
-				<></>
-			)}
-			{caption ? (
-				slug ? (
-					<Link to={"/" + slug}>
-						<figcaption className={styles.description}>
-							<span>{caption}</span>
-						</figcaption>
-					</Link>
-				) : (
-					<figcaption className={styles.description}>
-						<span>{caption}</span>
-					</figcaption>
-				)
-			) : (
-				<></>
-			)}
-		</figure>
-	)
+			)
+		} else if (
+			mediaType === "image/jpeg" ||
+			mediaType === "image/png" ||
+			mediaType === "image/webp" ||
+			mediaType === "image/gif" ||
+			mediaType === "image/svg"
+		) {
+			return <GatsbyImage image={img} alt="" />
+		} else return <></>
+	}
+)
+
+interface ThumbnailProps {
+	caption?: string
+	hidden?: boolean
+	image: any
+	ref?: React.ForwardedRef<HTMLDivElement>
+	slug?: string
+	type: string
+	width?: number
 }
+
+const Thumbnail: React.FC<ThumbnailProps> = React.forwardRef(
+	({ caption, image, hidden, slug, type, width }, ref) => {
+		// const thumbnail = React.useRef<HTMLDivElement>(null)
+		const video = React.useRef<HTMLVideoElement>(null)
+
+		// const [visible, setVisible] = React.useState(true)
+
+		// React.useEffect(() => {
+		// 	const observer = new IntersectionObserver((entries) => {
+		// 		entries.forEach((entry) => {
+		// 			if (entry.isIntersecting) {
+		// 				setVisible(true)
+		// 			} else {
+		// 				setVisible(false)
+		// 			}
+		// 		})
+		// 	})
+
+		// 	if (thumbnail.current) {
+		// 		observer.observe(thumbnail.current)
+		// 	}
+
+		// 	return () => {
+		// 		if (thumbnail.current) {
+		// 			observer.unobserve(thumbnail.current)
+		// 		}
+		// 	}
+		// }, [])
+
+		return (
+			<figure
+				className={styles.thumbnail}
+				hidden={hidden}
+				ref={ref}
+				style={{
+					flexBasis: `min(min(${
+						width ? width : 1
+					} * var(--column-width), 75vw), 50vh)`,
+				}}
+			>
+				{slug ? (
+					<>
+						<Image img={image} mediaType={type} />
+						{caption && (
+							<Link to={"/" + slug}>
+								<figcaption className={styles.description}>
+									<span>{caption}</span>
+									<span>↗</span>
+								</figcaption>
+							</Link>
+						)}
+					</>
+				) : (
+					<>
+						<Image img={image} mediaType={type} />
+						{caption && (
+							<figcaption className={styles.description}>
+								<span>{caption}</span>
+								<span>↗</span>
+							</figcaption>
+						)}
+					</>
+				)}
+			</figure>
+		)
+	}
+)
 
 export default Thumbnail
