d5485f79f38f591dec10c602ddb4e4a5eabd43e4
fabilou <hello@fabilou.eu>
2025-03-07 17:38:31 +0100

    Update Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { graphql, Link, useStaticQuery } from "gatsby"
 
 import { useGlobalContext } from "./GlobalContext"
 import Thumbnail from "./Thumbnail"
@@ -7,93 +6,12 @@ import ProjectInfo from "./ProjectInfo"
 
 import * as styles from "./Projects.module.sass"
 
-interface ProjectsProps {
-	filter?: string
-}
-
-const Projects: React.FC<ProjectsProps> = ({ filter }) => {
-	const data = useStaticQuery(graphql`
-		query {
-			allGalleryYaml {
-				edges {
-					node {
-						slug
-						thumbnail {
-							childImageSharp {
-								gatsbyImageData
-							}
-							internal {
-								mediaType
-							}
-							publicURL
-						}
-						title
-						width
-					}
-				}
-			}
-		}
-	`)
-
-	const { projectInfo } = useGlobalContext()
+const Projects: React.FC = () => {
+	const { project } = useGlobalContext()
 
 	const containerRef = React.useRef<HTMLDivElement>(null)
 	const thumbnailRef = React.useRef<HTMLDivElement>(null)
 	const infoRef = React.useRef<HTMLDivElement>(null)
-
-	React.useEffect(() => {
-		const container = containerRef.current
-		if (!container) return
-
-		container.scrollLeft = 0
-	}, [filter])
-
-	// React.useEffect(() => {
-	// 	const container = containerRef.current
-	// 	if (!container) return
-
-	// 	const handleWheel = (e: WheelEvent) => {
-	// 		container.scrollLeft += e.deltaY
-	// 	}
-
-	// 	container.addEventListener("wheel", handleWheel)
-
-	// 	return () => {
-	// 		container.removeEventListener("wheel", handleWheel)
-	// 	}
-	// }, [])
-
-	// React.useEffect(() => {
-	// 	const info = infoRef.current
-	// 	if (!info) return
-
-	// 	const handleMouseEnter = (e: React.MouseEvent) => {
-	// 		containerRef.current?.removeEventListener("wheel", () => {})
-	// 	}
-
-	// 	info.addEventListener("wheel", handleWheel)
-
-	// 	return () => {
-	// 		info.removeEventListener("wheel", handleMouseEnter)
-	// 	}
-	// }, [])
-
-	// React.useEffect(() => {
-	// 	const observer = new IntersectionObserver((entries) => {
-	// 		console.log(entries.map((entry) => entry.target))
-	// 	})
-
-	// 	if (thumbnailRef.current) {
-	// 		console.log(thumbnailRef.current)
-	// 		observer.observe(thumbnailRef.current)
-	// 	}
-
-	// 	return () => {
-	// 		if (thumbnailRef.current) {
-	// 			observer.unobserve(thumbnailRef.current)
-	// 		}
-	// 	}
-	// }, [])
 
 	const [grabbing, setGrabbing] = React.useState(false)
 	const [scrollStart, setScrollStart] = React.useState(0)
@@ -135,38 +53,32 @@ const Projects: React.FC<ProjectsProps> = ({ filter }) => {
 				onMouseLeave={(e) => handleMouseLeave(e)}
 				onMouseMove={(e) => handleMouseMove(e)}
 			>
-				{projectInfo !== null ? (
+				{project?.projectInfo && (
 					<ProjectInfo
-						description={projectInfo.description}
-						info={projectInfo.info}
-						links={projectInfo.links}
+						description={project.projectInfo.description}
+						info={project.projectInfo.info}
+						links={project.projectInfo.links}
 						ref={infoRef}
-						tags={projectInfo.tags}
-						title={projectInfo.title}
+						tags={project.projectInfo.tags}
+						title={project.projectInfo.title}
 					/>
-				) : (
-					<></>
 				)}
-				{data.allGalleryYaml.edges.map((item: any, i: number) => (
-					<Thumbnail
-						caption={
-							item.node.title && item.node.slug !== filter
-								? item.node.title
-								: null
-						}
-						hidden={filter !== undefined && item.node.slug !== filter}
-						image={
-							item.node.thumbnail.childImageSharp
-								? item.node.thumbnail.childImageSharp.gatsbyImageData
-								: item.node.thumbnail.publicURL
-						}
-						key={i}
-						ref={thumbnailRef}
-						slug={item.node.slug ? item.node.slug : null}
-						type={item.node.thumbnail.internal.mediaType}
-						width={item.node.width}
-					/>
-				))}
+				{project?.media &&
+					project.media.map((item: any, i: number) => (
+						<Thumbnail
+							caption={item.link && item.link.title}
+							image={
+								item.path.childImageSharp
+									? item.path.childImageSharp.gatsbyImageData
+									: item.path.publicURL
+							}
+							key={i}
+							ref={thumbnailRef}
+							slug={item.link && item.link.path}
+							type={item.path.internal.mediaType}
+							width={item.width}
+						/>
+					))}
 			</div>
 		</div>
 	)
